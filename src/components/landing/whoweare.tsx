8fc82959b441cc0588e522e47638f712faa84946
rubyaj24 <amaljithmvinod@gmail.com>
2025-03-28 23:44:18 +0530

    Update "Who We Are" section heading and adjust card dimensions

--- src/components/landing/whoweare.tsx
+++ src/components/landing/whoweare.tsx
@@ -5,13 +5,13 @@ const WhoWeAreSection: React.FC = () => {
   return (
     <section className="flex overflow-hidden flex-col items-center px-20 pt-4 pb-20 mt-8 w-full bg-zinc-100 max-md:px-5 max-md:max-w-full">
       <div className="flex flex-col w-full max-w-[1226px] max-md:max-w-full">
-      Who
-        <h2 className="self-center text-4xl font-bold text-black">
-           We Are
+        <h2 className="self-center text-4xl font-medium text-black">
+           Who <span className="font-bold">You are?</span>
         </h2>
         <div className="flex flex-wrap gap-5 justify-center mt-14 max-md:mt-10 max-md:max-w-full">
-          <div className="w-[570px] h-[240px] p-14 bg-white rounded-[30px] relative overflow-hidden shadow-sm group transition-all duration-300 hover:shadow-md">
-            <p className="text-base text-center">
+          <div className="w-[570px] h-[180px] p-14 bg-white rounded-[30px] relative overflow-hidden shadow-sm group transition-all duration-300 hover:shadow-md">
+            <p className="text-small text-center items-center justify-center
+            ">
               We are committed to raising awareness against 
               drug abuse through education, media 
               campaigns, and active community participation .
@@ -19,8 +19,8 @@ const WhoWeAreSection: React.FC = () => {
             <div className="absolute bottom-0 left-0 w-20 h-20 bg-green-400 rounded-tr-full transition-transform duration-500 ease-out group-hover:scale-[1.75]"></div>
           </div>
           
-          <div className="w-[570px] h-[240px] p-14 bg-white rounded-[30px] relative overflow-hidden shadow-sm group transition-all duration-300 hover:shadow-md">
-            <p className="text-base text-center">
+          <div className="w-[570px] h-[180px] p-14 bg-white rounded-[30px] relative overflow-hidden shadow-sm group transition-all duration-300 hover:shadow-md">
+            <p className="text-small text-center">
             Our initiative involves sensitization programs for students, parents, and teachers, along with peer mentoring, community engagement, and rehabilitation support. 
             </p>
             <div className="absolute bottom-0 left-0 w-20 h-20 bg-green-400 rounded-tr-full transition-transform duration-500 ease-out group-hover:scale-[1.75]"></div>
